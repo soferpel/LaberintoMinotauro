0f02d78ee50fb0518167b29a72e3d4d49ddd98e9
InakiRuizDeAlegria <i.ruizdealegria@opendeusto.es>
2024-05-04 00:14:09 +0200

    bug camara solucionado

--- Assets/Scripts/MovimientoPersonaje.cs
+++ Assets/Scripts/MovimientoPersonaje.cs
@@ -4,18 +4,15 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 5f; // Velocidad de movimiento del jugador
+    public float speed = 5f;
+    public Transform camTransform; 
 
     void Update()
     {
-        // Obtener la entrada del teclado
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
-
-        // Calcular el desplazamiento basado en la entrada y la velocidad
-        Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput) * speed * Time.deltaTime;
-
-        // Aplicar el desplazamiento al jugador
+        Vector3 camForward = Vector3.Scale(camTransform.forward, new Vector3(1, 0, 1)).normalized;
+        Vector3 movement = (verticalInput * camForward + horizontalInput * camTransform.right).normalized * speed * Time.deltaTime;
         transform.Translate(movement, Space.World);
     }
 }
