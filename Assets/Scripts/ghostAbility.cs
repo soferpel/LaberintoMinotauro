bdaf6bfcefe075302aff48d30233d6f47d0fc8a1
InakiRuizDeAlegria <i.ruizdealegria@opendeusto.es>
2024-05-04 19:55:15 +0200

    habilidad fantasma creada

---
+++ Assets/Scripts/ghostAbility.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ghostAbility : MonoBehaviour
+{
+    public float time = 3f;
+    private bool activate = false;
+    private List<Collider> ignorerWalls = new List<Collider>();
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            StartCoroutine(ghostActivate());
+        }
+    }
+
+    IEnumerator ghostActivate()
+    {
+        activate = true;
+
+        yield return new WaitForSeconds(time);
+
+        activate = false;
+
+        finishAbility();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (activate)
+        {
+            Physics.IgnoreCollision(collision.collider, GetComponent<Collider>());
+            ignorerWalls.Add(collision.collider);
+            Debug.Log(collision.collider);
+        }
+    }
+
+    void finishAbility()
+    {
+        foreach (Collider wall in ignorerWalls)
+        {
+            if (wall != null)
+            {
+                Physics.IgnoreCollision(wall, GetComponent<Collider>(), false);
+            }
+        }
+        ignorerWalls.Clear();
+    }
+
+
+}
