2dd7d701bf8e0c631fb8be911d052045902b3800
soferpel <so.fernandez@opendeusto.es>
2024-06-02 00:36:39 +0200

    sensibilidad por favot

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Camera : MonoBehaviour
 {
-    public float sensitivity = 50.0f;
+    public float sensitivity = 20.0f;
     public Transform playerBody;
 
     float xRotation = 0.0f;
