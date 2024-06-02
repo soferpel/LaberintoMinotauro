82c8d66d424cb4809709bc1a66f9ecc856a36d4e
InakiRuizDeAlegria <i.ruizdealegria@opendeusto.es>
2024-06-02 13:26:50 +0200

    menu pausa

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenuUI;
+    public AudioSource backgroundMusic;
+    private bool isPaused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        backgroundMusic.Play();
+        isPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        backgroundMusic.Pause();
+        isPaused = true;
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("FSEF");
+        Application.Quit();
+    }
+}
