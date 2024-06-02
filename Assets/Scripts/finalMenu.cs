1377cd1f58e0d2f325f96f21683296f482b88054
InakiRuizDeAlegria <i.ruizdealegria@opendeusto.es>
2024-06-02 17:59:52 +0200

    arreglo menu fin

--- Assets/Scripts/finalMenu.cs
+++ Assets/Scripts/finalMenu.cs
@@ -5,9 +5,31 @@ using UnityEngine.SceneManagement;
 
 public class finalMenu : MonoBehaviour
 {
+    private bool isInMenu = true;
+
+    void Update()
+    {
+        if (isInMenu)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+
+    public void GameScene()
+    {
+        isInMenu = false;
+        SceneManager.LoadScene("MainMenuInfo");
+    }
+
     public void Quit()
     {
+        isInMenu = false;
         Application.Quit();
-
     }
 }
